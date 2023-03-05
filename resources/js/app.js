13d61886ef9e5b9805e6e619fe5b399a5f7bbf72
olegopro <evilgazz@gmail.com>
2023-03-05 13:20:53 +0300

    Подключил perfect scrollbar

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,11 +2,14 @@ import { createApp } from 'vue'
 import router from './router'
 import App from './App.vue'
 import store from './store'
+import PerfectScrollbar  from 'vue3-perfect-scrollbar'
+import 'vue3-perfect-scrollbar/dist/vue3-perfect-scrollbar.css'
 
 require('./bootstrap')
 
 createApp(App)
     .use(store)
     .use(router)
+    .use(PerfectScrollbar)
     .mount('#app')
 
